0ecb60bdf8b9053979c6645aebdc0fceff747ee7
Ryan Gerstenkorn <me@ryanjarv.sh>
2022-03-28 18:32:09 -0700

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-with_vulnerabilities="$3"
 OUTDIR="/data"
-if [[ "$with_vulnerabilities" == "true" ]]; then
-  echo "Scanning with vulners script ..."
-  nmap -sV --script vulners --script-args mincvss=5.0 -oA "$OUTDIR/$1" "$2"
-else
-  echo "Scanning without vulners script ..."
-  nmap -sT -oA "$OUTDIR/$1" "$2"
-fi
+echo "Running: nmap -oA $OUTDIR/$1 $@"
+nmap -oA "$OUTDIR/$1" "$@"
 
 # Convert XML to HTML report
 xsltproc -o "$OUTDIR/$1.html" /tmp/nmap-bootstrap.xsl "$OUTDIR/$1.xml"
